6662e2944df06156b9751c6599bb0cff5bbab350
Abhishek Korra <abhishek.k@avenirdigital.ai>
2019-12-09 10:45:57 +0530

    File renames

--- bert_hub.py
+++ bert_hub.py
@@ -1,128 +1,195 @@
 
+from sklearn.model_selection import train_test_split
+import os
+import pandas as pd
+import re
 import tensorflow as tf
-from datetime import datetime
+import tensorflow_hub as hub
 
+from tensorflow import keras
+from bert import optimization
 from bert import run_classifier
+from bert import tokenization
 
-from bert_hub_server import load_dataset, split_into_train_test, load_from_folder,\
-	create_tokenizer_from_hub_module, model_fn_builder, getPrediction
-
-
-# CSV_FILE = "data/data.csv"
-train_folder = "imdb/train"
-test_folder = "imdb/test"
-LABELS = ['pos', 'neg']
-DATA_COLUMN = "sentence"
-LABEL_COLUMN = "label"
-
-OUTPUT_DIR = "output_dir/"
-BERT_MODEL_HUB = "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
-
-MAX_SEQ_LENGTH = 128
-BATCH_SIZE = 32
-LEARNING_RATE = 2e-5
-NUM_TRAIN_EPOCHS = 3.0
-WARMUP_PROPORTION = 0.1
-# Model configs
-SAVE_CHECKPOINTS_STEPS = 500
-SAVE_SUMMARY_STEPS = 100
-
-# dataset = load_dataset(CSV_FILE)
-# train, test = split_into_train_test(dataset)
-print("[INFO] Loading data from folders...")
-train = load_from_folder(train_folder, labels=LABELS,
-	data_column=DATA_COLUMN, label_column=LABEL_COLUMN)
-test = load_from_folder(test_folder, labels=LABELS,
-	data_column=DATA_COLUMN, label_column=LABEL_COLUMN)
-print("[INFO] Done loading data...\n")
-
-train = train.sample(5000)
-test = test.sample(5000)
-
-label_list = train[LABEL_COLUMN].unique()
-
-print("[INFO] Preparing InputExample...")
-train_inputExamples = train.apply(
-	lambda x: run_classifier.InputExample(
-		guid=None,
-		text_a=x[DATA_COLUMN],
-		text_b=None,
-		label=x[LABEL_COLUMN]), axis=1)
-
-test_inputExamples = test.apply(
-	lambda x: run_classifier.InputExample(
-		guid=None,
-		text_a=x[DATA_COLUMN],
-		text_b=None,
-		label=x[LABEL_COLUMN]), axis=1)
-print("[INFO] Done preparing InputExample...\n")
-
-print("[INFO] Preparing Tokenizer...")
-tokenizer = create_tokenizer_from_hub_module(BERT_MODEL_HUB)
-print("[INFO] Done preparing tokenizer...\n")
-
-print("[INFO] Preparing features...")
-train_features = run_classifier.convert_examples_to_features(
-	train_inputExamples, label_list, MAX_SEQ_LENGTH, tokenizer)
-test_features = run_classifier.convert_examples_to_features(
-	test_inputExamples, label_list, MAX_SEQ_LENGTH, tokenizer)
-print("[INFO] Done preparing features...\n")
+def load_dataset(csv_file):
+	pass
 
 
-num_train_steps = int(len(train_features) / BATCH_SIZE * NUM_TRAIN_EPOCHS)
-num_warmup_steps = int(num_train_steps * WARMUP_PROPORTION)
+def split_into_train_test(df):
+	pass
 
-# Specify outpit directory and number of checkpoint steps to save
-run_config = tf.estimator.RunConfig(
-    model_dir=OUTPUT_DIR,
-    save_summary_steps=SAVE_SUMMARY_STEPS,
-    save_checkpoints_steps=SAVE_CHECKPOINTS_STEPS)
 
-model_fn = model_fn_builder(
-	bert_model_hub=BERT_MODEL_HUB,
-	num_labels=len(label_list),
-	learning_rate=LEARNING_RATE,
-	num_train_steps=num_train_steps,
-	num_warmup_steps=num_warmup_steps)
+def load_from_folder(path, labels, data_column, label_column):
+	i = 0
+	data = {'sentence': [], 'label': []}
+	for folder in labels:
+		folder_path = os.path.join(path, folder)
+		for file in os.listdir(folder_path):
+			file_path = os.path.join(folder_path, file)
+			with tf.gfile.GFile(file_path, "r") as f:
+				data[data_column].append(f.read())
+				data[label_column].append(i)
+		i += 1
+	return pd.DataFrame.from_dict(data).reset_index(drop=True)
 
-estimator = tf.estimator.Estimator(
-	model_fn=model_fn,
-	config=run_config,
-	params={"batch_size": BATCH_SIZE})
 
-train_input_fn = run_classifier.input_fn_builder(
-    features=train_features,
-    seq_length=MAX_SEQ_LENGTH,
-    is_training=True,
-    drop_remainder=False)
+def create_tokenizer_from_hub_module(bert_model_hub):
+	with tf.Graph().as_default():
+		bert_module = hub.Module(bert_model_hub)
+		tokenization_info = bert_module(signature='tokenization_info', as_dict=True)
+		with tf.Session() as sess:
+			vocab_file, do_lower_case = sess.run([tokenization_info['vocab_file'],
+												  tokenization_info['do_lower_case']])
+	return tokenization.FullTokenizer(
+		vocab_file=vocab_file, do_lower_case=do_lower_case)
 
-print(f"[INFO] No. of train steps: {num_train_steps}")
-print(f"[INFO] No. of warmup steps: {num_warmup_steps}")
 
-print(f'[INFO] Beginning Training...!')
-current_time = datetime.now()
-estimator.train(input_fn=train_input_fn, max_steps=num_train_steps)
-print(f"[INFO] Training took time {datetime.now() - current_time} sec...!\n")
+def create_model(bert_model_hub, is_predicting, input_ids, input_mask, segment_ids, labels,
+				 num_labels):
+	"""Creates a classification model."""
 
-test_input_fn = run_classifier.input_fn_builder(
-    features=test_features,
-    seq_length=MAX_SEQ_LENGTH,
-    is_training=False,
-    drop_remainder=False)
+	bert_module = hub.Module(bert_model_hub, trainable=True)
+	bert_inputs = dict(
+		input_ids=input_ids,
+		input_mask=input_mask,
+		segment_ids=segment_ids)
+	bert_outputs = bert_module(
+		inputs=bert_inputs,
+		signature="tokens",
+		as_dict=True)
 
-print(f'[INFO] Beginning evaluating...!')
-estimator.evaluate(input_fn=test_input_fn, steps=None)
-print(f"[INFO] Done evaluating...\n")
+	# Use "pooled_output" for classification tasks on an entire sentence.
+	# Use "sequence_outputs" for token-level output.
+	output_layer = bert_outputs["pooled_output"]
 
-pred_sentences = [
-  "That movie was absolutely awful",
-  "The acting was a bit lacking",
-  "The film was creative and surprising",
-  "Absolutely fantastic!"
-]
-print(f"[INFO] Beginning predicting...")
-predictions = getPrediction(
-	estimator, pred_sentences, labels=LABELS, label_list=label_list,
-	max_seq_len=MAX_SEQ_LENGTH, tokenizer=tokenizer)
-print(f"[INFO] Done predicting...\n")
-print(predictions)
+	hidden_size = output_layer.shape[-1].value
+
+	# Create our own layer to tune for politeness data.
+	output_weights = tf.get_variable(
+		"output_weights", [num_labels, hidden_size],
+		initializer=tf.truncated_normal_initializer(stddev=0.02))
+
+	output_bias = tf.get_variable("output_bias", [num_labels],
+								  initializer=tf.zeros_initializer())
+
+	with tf.variable_scope("loss"):
+
+		# Dropout helps prevent overfitting
+		output_layer = tf.nn.dropout(output_layer, keep_prob=0.9)
+
+		logits = tf.matmul(output_layer, output_weights, transpose_b=True)
+		logits = tf.nn.bias_add(logits, output_bias)
+		log_probs = tf.nn.log_softmax(logits, axis=-1)
+
+		# Convert labels into one-hot encoding
+		one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
+
+		predicted_labels = tf.squeeze(tf.argmax(log_probs, axis=-1, output_type=tf.int32))
+		# If we're predicting, we want predicted labels and the probabiltiies.
+		if is_predicting:
+		  return (predicted_labels, log_probs)
+
+		# If we're train/eval, compute loss between predicted and actual label
+		per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
+		loss = tf.reduce_mean(per_example_loss)
+		return (loss, predicted_labels, log_probs)
+
+
+def model_fn_builder(bert_model_hub, num_labels, learning_rate, num_train_steps,
+					 num_warmup_steps):
+	"""Returns `model_fn` closure for TPUEstimator."""
+	def model_fn(features, labels, mode, params):  # pylint: disable=unused-argument
+		"""The `model_fn` for TPUEstimator."""
+
+		input_ids = features["input_ids"]
+		input_mask = features["input_mask"]
+		segment_ids = features["segment_ids"]
+		label_ids = features["label_ids"]
+
+		is_predicting = (mode == tf.estimator.ModeKeys.PREDICT)
+
+		# TRAIN and EVAL
+		if not is_predicting:
+
+			(loss, predicted_labels, log_probs) = create_model(bert_model_hub,
+				is_predicting, input_ids, input_mask, segment_ids,
+				label_ids, num_labels)
+
+			train_op = optimization.create_optimizer(
+				loss, learning_rate, num_train_steps, num_warmup_steps,
+				use_tpu=False)
+
+			# Calculate evaluation metrics. 
+			def metric_fn(label_ids, predicted_labels):
+				accuracy = tf.metrics.accuracy(label_ids, predicted_labels)
+				f1_score = tf.contrib.metrics.f1_score(
+					label_ids,
+					predicted_labels)
+				auc = tf.metrics.auc(
+					label_ids,
+					predicted_labels)
+				recall = tf.metrics.recall(
+					label_ids,
+					predicted_labels)
+				precision = tf.metrics.precision(
+					label_ids,
+					predicted_labels) 
+				true_pos = tf.metrics.true_positives(
+					label_ids,
+					predicted_labels)
+				true_neg = tf.metrics.true_negatives(
+					label_ids,
+					predicted_labels)
+				false_pos = tf.metrics.false_positives(
+					label_ids,
+					predicted_labels)  
+				false_neg = tf.metrics.false_negatives(
+					label_ids,
+					predicted_labels)
+				return {
+					"eval_accuracy": accuracy,
+					"f1_score": f1_score,
+					"auc": auc,
+					"precision": precision,
+					"recall": recall,
+					"true_positives": true_pos,
+					"true_negatives": true_neg,
+					"false_positives": false_pos,
+					"false_negatives": false_neg
+				}
+
+			eval_metrics = metric_fn(label_ids, predicted_labels)
+
+			if mode == tf.estimator.ModeKeys.TRAIN:
+				return tf.estimator.EstimatorSpec(
+					mode=mode, loss=loss, train_op=train_op)
+			else:
+				return tf.estimator.EstimatorSpec(
+					mode=mode, loss=loss, eval_metric_ops=eval_metrics)
+		else:
+			(predicted_labels, log_probs) = create_model(bert_model_hub,
+				is_predicting, input_ids, input_mask, segment_ids,
+				label_ids, num_labels)
+
+			predictions = {
+				'probabilities': log_probs,
+				'labels': predicted_labels
+			}
+			return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+
+	return model_fn
+
+
+def getPrediction(estimator, in_sentences, labels, label_list, max_seq_len,
+				  tokenizer):
+	input_examples = [run_classifier.InputExample(
+		guid="", text_a = x, text_b = None, label = 0) for x in in_sentences]
+	input_features = run_classifier.convert_examples_to_features(
+		input_examples, label_list, max_seq_len, tokenizer)
+	predict_input_fn = run_classifier.input_fn_builder(
+		features=input_features, seq_length=max_seq_len, is_training=False,
+		drop_remainder=False)
+	predictions = estimator.predict(predict_input_fn)
+	return [(sentence, prediction['probabilities'],
+			 labels[prediction['labels']])
+			for sentence, prediction in zip(in_sentences, predictions)]
